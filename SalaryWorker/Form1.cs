a3224ce3eb9fdddf0a8a160002b0fa87bb7209ae
yuriy070920 <surzhickov.yura37@gmail.com>
2020-04-10 11:44:43 +0300

    test commit

--- SalaryWorker/Form1.cs
+++ SalaryWorker/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Npgsql;
 
 namespace SalaryWorker
 {
@@ -15,6 +16,11 @@ namespace SalaryWorker
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
